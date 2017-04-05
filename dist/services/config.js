bf52205f5b61df3a393d074ee2e12b409796bed9
Stephane Meaudre <smeaudre@kaliop.com>
2017-04-05 13:31:00 +0200

    Adding config

---
+++ dist/services/config.js
@@ -0,0 +1,8 @@
+export default {
+    kuzzleUrl: 'localhost',
+    kuzzleIndex: '<create_index>',
+    projectionFrom: 'EPSG:3857',
+    projectionTo: 'EPSG:4326',
+    latDefault: 48.856614,
+    longDefault: 2.352222,
+}
---
+++ dist/services/kuzzle.js
@@ -0,0 +1,22 @@
+/**
+ * Connextion to Kuzzle
+ */
+import Kuzzle from 'kuzzle-sdk'
+import config from './config'
+
+var optionsConnect = {
+    defaultIndex: config.kuzzleIndex,
+    autoReconnect: true,
+    port : 7512,
+    headers: {
+        'Access-Control-Allow-Origin' : '*'
+    }
+};
+
+var kuzzle = new Kuzzle(config.kuzzleUrl, optionsConnect, function(err, res) {
+    if (err) {
+        console.log(err.message);
+    }
+});
+
+export default kuzzle;
